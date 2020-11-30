36f253fb7295a7a1fca28bfe4a88f227dfff6145
vugutsa <vugutsamercy84@gmail.com>
2020-11-30 13:10:23 +0300

    put the get_news function so as to give our base url a response

--- app/requests.py
+++ app/requests.py
@@ -1,27 +1,27 @@
-# import urllib.request,json
-# from .models import News
-# # Getting api key
-# api_key = None
-# # Getting the movie base url
-# base_url = None
-# def configure_request(app):
-#     global api_key,base_url
-#     api_key = app.config['NEWS_API_KEY']
-#     base_url = app.config['NEWS_API_BASE_URL']
-# def get_news(category):
-#     '''
-#     Function that gets the json response to our url request
-#     '''
-#     get_news_url = base_url.format(category,api_key)
-#     with urllib.request.urlopen(get_news_url) as url:
-#         get_news_data = url.read()
-#         get_news_response = json.loads(get_news_data)
-#         news_results = None
-#         if get_news_response['articles']:
-#             news_articles_list = get_news_response['articles']
-#             news_results = process_results(news_articles_list)
-#     # print("Result",news_results)
-#     return news_results
+import urllib.request,json
+from .models import News
+# Getting api key
+api_key = None
+# Getting the movie base url
+base_url = None
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+        news_results = None
+        if get_news_response['articles']:
+            news_articles_list = get_news_response['articles']
+            news_results = process_results(news_articles_list)
+    # print("Result",news_results)
+    return news_results
 # def process_results(news_list):
 #     '''
 #     Function  that processes the news result and transform them to a list of Objects
